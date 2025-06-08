e6dd8a6b64c043a2ef3f5b30cea31e28b36c0577
keberth <keberthjosera@msmk.university>
2025-06-08 15:01:29 +0200

    function showAllRecipes funcional; probada y ahora carga ids. NO MUESTRA UNIDADES todavía. Agregada la ruta relativa en filename (inicio de main).

--- main.cpp
+++ main.cpp
@@ -7,25 +7,26 @@
 
 using json = nlohmann::json;
 
-// Principal class to represent recipes
 // => Encapsulates recipe data on objects
 class Recipe
 {
 public:
+  int id;
   std::string recipe_name;
   std::map<std::string, double> ingredients;
   std::string steps_preparation;
 
-  Recipe(const std::string &recipe_name,
+  Recipe(const int id,
+         std::string &recipe_name,
          const std::map<std::string, double> &ingredients,
          const std::string &steps_preparation)
-      : recipe_name(recipe_name),
+      : id(id),
+        recipe_name(recipe_name),
         ingredients(ingredients),
         steps_preparation(steps_preparation) {}
 };
 
 // Class to manage ingredients (basic structure)
-// => Encapsulates ingredient data on legible objects
 class Ingredient
 {
 public:
@@ -42,30 +43,33 @@ public:
 class RecipeManager
 {
 private:
-  // Collection of recipes
   std::vector<Recipe> recipes;
 
 public:
-  // => shows avilable recipes based on ingredients loaded.
-  void showRecipes() const;
+  void showAvilableRecipes() const;
 
-  // => shows all recipes loaded in the manager
   void showAllRecipes() const;
 
-  // => loads recipes from json on program startup
   void loadRecipesFromJson(const std::string &filename);
 };
+
+/* Methods are defined beneath this comment
+ - showRecipes() => Shows available recipes based on loaded ingredients.
+ - showAllRecipes() => Shows all recipes loaded in the program.
+ - loadRecipesFromJson() => Loads recipes from json on program startup.
+*/
 
 void RecipeManager::loadRecipesFromJson(const std::string &filename)
 {
 
-  /* glosary:
+  /*glosary:
   - recfile => Recipe file
   - recipes => Our collection of recipes seen above
   - j => json object to parse the file
   - loadRec => Each recipe loaded from the json file to the program
-  - ingrJson => Each ingredient loaded from the json file to the program
   - Qty, Qties => Quantity, Quantities => ingrQty means the quantity of the ingredient
+  - recIngrMap => Map of ingredients for each recipeq
+  - ingr (short for ingredient) =>
   */
   std::ifstream recfile(filename);
   if (!recfile.is_open())
@@ -81,6 +85,7 @@ void RecipeManager::loadRecipesFromJson(const std::string &filename)
 
   for (const auto &loadRec : j)
   {
+    int recipeId = loadRec["id"];
     std::string recipeName = loadRec["name"];
     std::string recipeSteps = loadRec["instructions"];
 
@@ -90,9 +95,46 @@ void RecipeManager::loadRecipesFromJson(const std::string &filename)
     {
       std::string ingrName = ingrJson["name"];
       double ingrQty = ingrJson["quantity"];
+      std::string ingrUnit = ingrJson["unit"];
+
       recIngrMap[ingrName] = ingrQty;
     }
 
-    recipes.emplace_back(recipeName, recIngrMap, recipeSteps);
+    recipes.emplace_back(recipeId, recipeName, recIngrMap, recipeSteps);
   }
 };
+
+void RecipeManager::showAvilableRecipes() const
+{
+  std::cout << "Available Recipes:" << std::endl;
+};
+
+void RecipeManager::showAllRecipes() const
+{
+  std::cout << "Available Recipes:" << std::endl;
+  for (const auto &recipe : recipes)
+  {
+    std::cout << "ID: " << recipe.id << std::endl;
+    std::cout << "Recipe: " << recipe.recipe_name << std::endl;
+    std::cout << "Ingredients:" << std::endl;
+
+    for (const auto &ingredient : recipe.ingredients)
+    {
+      const std::string &name = ingredient.first;
+      const double &quantity = ingredient.second;
+
+      std::cout << "- " << name << ": " << quantity << std::endl;
+    }
+    std::cout << "Steps: " << recipe.steps_preparation << std::endl;
+    std::cout << "------------------------" << std::endl;
+  }
+};
+
+int main()
+{
+  RecipeManager manager;
+  std::string filename = "../data/recipes.json";
+  manager.loadRecipesFromJson(filename);
+  manager.showAllRecipes();
+  return 0;
+}
