ca600e589406f6c365a0aba24b5b4c0e5a40a27f
keberth <keberthjosera@msmk.university>
2025-06-11 21:06:35 +0200

    code refraction ACTUALLY WORKING LETSGOOOOOO; deleted class mainmenu, instead, used a fun. Methods of all classes were updates to work with the rest of the code. Still no documentation

--- main.cpp
+++ main.cpp
@@ -6,12 +6,18 @@
 #include <map>
 #include <vector>
 #include "imports/nlohmann/json.hpp"
-/*TODO list
-  TODO: manuallyAddIngredients
-  TODO: menu list (prob another class)
-  TODO: ui (another class lmao)
-  TODO: proper documentation
+
+/*MAJOR CHANGES
+  - Code refraction: deleted MainMenu class; not necessary to be a class neither a fun.
+  - Consistence: all code was reviewed so IN THEORY all names of everyting is more accurate and "correct"
+  - Minor corrections: THE MAIN MENU NOW RETURN ITSELF TO THE MAIN MENU WHEN YOU EXIT THE SUBMENUS WOW!
+
+  Structure is pretty much the same, some references can be updated in order to make the program less heavy on load
+  bc it is not build thinking you can load over 9000 recipes or 16000 ingredients at a time.
+
+  TODO: save added ingredients onto my ingredientd.txt file
 */
+
 using json = nlohmann::json;
 
 std::string trim(const std::string &s)
@@ -70,24 +76,12 @@ private:
 public:
   void loadIngredientsFromFile(const std::string &filename);
   void manuallyAddIngredients();
+  void showAllIngredients();
 
   void loadRecipesFromJson(const std::string &filename);
+  void showAllRecipes();
   void showAvailableRecipes();
   void selectRecipe();
-
-  void showAllIngredients();
-  void showAllRecipes();
-};
-
-class MainMenu
-{
-public:
-  void startupMenu();
-  void recipeMenu();
-  void ingredientsMenu();
-
-  void selectRecipe();
-  void editIngredient();
 };
 
 // Methos beneath this comment
@@ -119,11 +113,26 @@ void RecipeManager::loadIngredientsFromFile(const std::string &filename)
   }
 };
 
-void RecipeManager::manuallyAddIngredients() {};
+void RecipeManager::manuallyAddIngredients()
+{
+  std::string name, unit;
+  int quantity;
+
+  std::cout << "Ingredient Name: ";
+  std::cin >> name;
+  std::cout << "Quantity: ";
+  std::cin >> quantity;
+  std::cout << "Unit (make sure to use plural; mililiters, grams or spoons): ";
+  std::cin >> unit;
+
+  ingredients.emplace_back(trim(name), quantity, trim(unit));
+  std::cout << "Ingredient" << name << " added succesfully!\n";
+};
 
 void RecipeManager::showAllIngredients()
 {
-  std::cout << "--- Ingredients loaded ---" << std::endl;
+  std::cout << "\nShowing ALL ingredients on program\n"
+            << std::endl;
 
   for (const auto &ingredient : ingredients)
   {
@@ -131,6 +140,7 @@ void RecipeManager::showAllIngredients()
               << ": Quantity: " << ingredient.quantity
               << " " << ingredient.unit << std::endl;
   }
+  std::cout << std::endl;
 }
 
 void RecipeManager::loadRecipesFromJson(const std::string &filename)
@@ -167,26 +177,35 @@ void RecipeManager::loadRecipesFromJson(const std::string &filename)
 
 void RecipeManager::showAllRecipes()
 {
-  std::cout << "--- Recipes loaded ---" << std::endl;
+  std::cout << "\nShowing ALL recipes on database\n"
+            << std::endl;
 
   for (const auto &recipe : recipes)
   {
     std::cout << "  Recipe ID: " << recipe.id << std::endl;
     std::cout << "  Name: " << recipe.recipe_name << std::endl;
-    std::cout << "  Ingredients:\n";
+    /*std::cout << "  Ingredients:\n";
     for (const auto &ing : recipe.ingredients)
     {
       std::cout << "    - " << ing.name << ": " << ing.quantity << " " << ing.unit << std::endl;
     }
     std::cout << "  Instructions: \n"
               << recipe.instructions << std::endl;
+    std::cout << std::endl;*/
+    std::cout << std::endl;
   }
-}
+};
 
 void RecipeManager::showAvailableRecipes()
 {
   std::vector<std::string> availableRecipes;
-  // SE NECESITA UNA LÓGICA PARA ESTANDARIZAR LAS UNIDADES
+  // la estandarización de unidades no existe, es un invento del gobierno para hacerme pensar de más
+  // GENERADO CON [COPILOT], SUJETO A CAMBIOS
+
+  std::cout << "\nShowing AVAILABLE Recipes\n"
+            << std::endl;
+  std::cout << "Available recipes with your ingredients, are: " << std::endl;
+
   for (const auto &recipe : recipes)
   {
     bool canMake = true;
@@ -211,27 +230,35 @@ void RecipeManager::showAvailableRecipes()
     }
     if (canMake)
     {
-      availableRecipes.push_back(recipe.recipe_name);
+      std::cout << "- " << recipe.recipe_name << std::endl;
     }
   }
 
-  std::cout << "Available recipes with your ingredients, are: " << std::endl;
   for (const auto &name : availableRecipes)
   {
     std::cout << "- " << name << std::endl;
   }
+  std::cout << std::endl;
 };
 
 void RecipeManager::selectRecipe()
 {
-  int choice = 0;
 
-  std::cout << "Your available recipes are:\n";
-  for (const auto &recipe : recipes)
+  if (recipes.empty())
   {
-    std::cout << "- " << recipe.recipe_name << std::endl;
+    std::cout << "DUMNbo" << std::endl;
+    return;
   }
-  std::cout << "Select a recipe: ";
+
+  std::cout << "\nAvailable recipes:\n"
+            << std::endl;
+  for (size_t i = 0; i < recipes.size(); ++i)
+  {
+    std::cout << i + 1 << ". " << recipes[i].recipe_name << std::endl;
+  }
+
+  int choice;
+  std::cout << "Select a recipe (number): ";
   std::cin >> choice;
 
   if (choice < 1 || choice > static_cast<int>(recipes.size()))
@@ -239,85 +266,20 @@ void RecipeManager::selectRecipe()
     std::cout << "Invalid selection." << std::endl;
     return;
   }
+
   const Recipe &selected = recipes[choice - 1];
-  std::cout << "Recipe ID: " << selected.id << std::endl;
-  std::cout << "Name: " << selected.recipe_name << std::endl;
+  std::cout << "\n--- " << selected.recipe_name << " ---" << std::endl;
   std::cout << "Ingredients:" << std::endl;
+
   for (const auto &ing : selected.ingredients)
   {
-    std::cout << "  - " << ing.name << ": " << ing.quantity << " " << ing.unit << std::endl;
+    std::cout << "- " << ing.name << ": "
+              << ing.quantity << " " << ing.unit << std::endl;
   }
-  std::cout << "Instructions:\n"
-            << selected.instructions << std::endl;
+
+  std::cout << "\nInstructions: " << selected.instructions << std::endl;
+  std::cout << std::endl;
 }
-
-void MainMenu::startupMenu()
-{
-  std::string choice;
-  std::cout << "Virtual Chef" << std::endl
-            << "--- ---" << std::endl;
-
-  std::cout << "1. Open Recipe Menu" << std::endl;
-  std::cout << "2. Open Ingredient Menu" << std::endl;
-  std::cout << "3. Exit Program" << std::endl;
-  std::cout << "Enter your choice: ";
-  std::getline(std::cin, choice);
-
-  switch (choice[0])
-  {
-  case '1':
-    recipeMenu();
-    break;
-  case '2':
-    ingredientsMenu();
-    break;
-  case '3':
-    std::cout << "Exiting program." << std::endl;
-    break;
-  }
-}
-
-void MainMenu::recipeMenu()
-{
-  std::cout << "Virtual Chef: Recipe Menu" << std::endl
-            << "--- ---" << std::endl;
-
-  std::cout << "1. Show All Recipes" << std::endl;
-  std::cout << "2. Show Available Recipes possibles with your current ingredients" << std::endl;
-  std::cout << "3. Select a Recipe" << std::endl;
-  std::cout << "4. Return to Main Menu" << std::endl;
-
-  std::string choice;
-  RecipeManager manager;
-
-  switch (choice[0])
-  {
-  case '1':
-    manager.showAllRecipes();
-    break;
-  case '2':
-    manager.showAvailableRecipes();
-    break;
-  case '3':
-    manager.selectRecipe();
-    break;
-  case '4':
-  }
-};
-
-void MainMenu::ingredientsMenu()
-{
-  std::cout << "Virtual Chef: Ingredient Menu" << std::endl
-            << "--- ---" << std::endl;
-
-  std::cout << "1. Show All Ingredients" << std::endl;
-  std::cout << "2. Edit Ingredients" << std::endl;
-  std::cout << "3. Return to Main Menu" << std::endl;
-};
-
-void RecipeManager::selectRecipe() {};
-
-void MainMenu::editIngredient() {};
 
 /*Execution seq:
 
@@ -343,36 +305,56 @@ void MainMenu::editIngredient() {};
   THIS IS ALL IN THEORY. FOR NOW, JUST THE FIRST 2 STEPS WORKS
 */
 
+void displayMenu()
+{
+  std::cout << "--- Virtual Chef ---" << std::endl;
+  std::cout << "1. Show all recipes" << std::endl;
+  std::cout << "2. Show available recipes" << std::endl;
+  std::cout << "3. Select a recipe" << std::endl;
+  std::cout << "4. Add ingredients manually" << std::endl;
+  std::cout << "5. Show all ingredients" << std::endl;
+  std::cout << "6. Exit" << std::endl;
+  std::cout << "Choose an option: ";
+}
+
 int main()
 {
-  RecipeManager manager;
-  std::string i = "../data/ingredients.txt";
-  std::string r = "../data/recipes.json";
+  RecipeManager rm;
+  const std::string i = "../data/ingredients.txt";
+  const std::string r = "../data/recipes.json";
 
-  try
-  {
-    manager.loadIngredientsFromFile(i);
-    manager.loadRecipesFromJson(r);
-    // manager.showAllIngredients();
-    // manager.showAllRecipes();
-    manager.showAvailableRecipes();
-  }
-  catch (const std::ifstream::failure &e)
-  {
-    std::cerr << "File operation failed: " << e.what() << '\n';
-  }
-  catch (const nlohmann::json::parse_error &e)
-  {
-    std::cerr << "JSON parse error: " << e.what() << '\n';
-  }
-  catch (const std::exception &e)
-  {
-    std::cerr << "An error occurred: " << e.what() << '\n';
-  }
-  catch (...)
-  {
-    std::cerr << "Unknown error.\n";
-  }
+  rm.loadIngredientsFromFile(i);
+  rm.loadRecipesFromJson(r);
 
+  int choice;
+  do
+  {
+    displayMenu(); // Displays full menu including prompt
+    std::cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+      rm.showAllRecipes();
+      break;
+    case 2:
+      rm.showAvailableRecipes();
+      break;
+    case 3:
+      rm.selectRecipe();
+      break;
+    case 4:
+      rm.manuallyAddIngredients();
+      break;
+    case 5:
+      rm.showAllIngredients();
+      break;
+    case 6:
+      std::cout << "Exiting program..." << std::endl;
+      break;
+    default:
+      std::cout << "Input a number between 1 and 6." << std::endl;
+    }
+  } while (choice != 6);
   return 0;
 }
